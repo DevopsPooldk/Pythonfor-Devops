e0916945bb94e9388752f993db0c680ca7bc2374
DevopsPooldk <dkhade323@gmil.com>
2024-08-14 18:21:20 +0530

    add p

---
+++ subpro.py
@@ -0,0 +1,9 @@
+a=150
+b=10
+sub=a-b
+print(sub)
+
+a=50
+b=50
+diff=a-b
+print(diff)
---
+++ sumof2no.py
@@ -0,0 +1,15 @@
+x = 5000
+y = 100
+sum = x + y
+print(sum)
+
+
+a=10
+b=-20
+sum=a+b
+print(sum)
+
+a=10.20
+b=100
+sum=a=+b
+print(sum)
