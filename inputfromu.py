5a4f229e6517cf16a517013c6255ced6218d916a
DevopsPooldk <dkhade323@gmil.com>
2024-08-14 21:13:03 +0530

    add inputs users

--- inputfromu.py
+++ inputfromu.py
@@ -1,12 +1,8 @@
 # taking input from user and print it 
-name = input("name : ")
-print(name)
-
-
-# taking integer type input from user and print it .
-age = int(input("age : "))
-print(age)
-
+name= input("name : ")
+# taking integer type input from user and print it 
+age= int(input("age :"))
 # Taking float type input from user and print it 
-learn = float(input("learn ; "))
-print(learn)
+learn= float(input("learn :"))
+
+print("My name is",name, "and i am ",age, "years old")
