beeef7977f7b0ae3238451a7b727e371a4c43b7a
DevopsPooldk <dkhade323@gmil.com>
2024-08-18 20:10:33 +0530

    add operator

---
+++ Operators/Arithmeticop.py
@@ -0,0 +1,11 @@
+# Operators and there type
+#1 Arithmetic Operator 
+
+a= 5
+b= 2
+print= (a+b)
+print= (a-b)
+print= (a*b)
+print= (a/b)
+print= (a%b) #modilo
+print= (a**b) #power operator
---
+++ Operators/relationalop.py
@@ -0,0 +1,9 @@
+# this for relational operator
+a=50
+b=20
+
+print= (a == b)
+print= (a!=b)
+print= (a > b)
+print= (a < b)
+print= (a >= b)
