6f567865f88e720faa194a0eff8268cdd96422a5
DevopsPooldk <dkhade323@gmil.com>
2024-08-18 20:48:53 +0530

    add new one

---
+++ Operators/assignmentop.py
@@ -0,0 +1,14 @@
+# assignment operator
+
+a=50
+b=10
+a += 10
+print("no :",a)
+
+
+
+num = 10
+num= num + 10 
+print(num)
+
+
---
+++ Operators/logicalop.py
@@ -0,0 +1,20 @@
+# logical operators
+
+#1 not 
+print(not False)
+print(not True)
+
+a=10
+b=20
+print(not(a>b))
+
+#2 and
+
+val1 =True
+val2 =True
+print(val1 and val2) 
+
+print(a>=b and b<a)
+
+# or
+print(a<b or a>b)
